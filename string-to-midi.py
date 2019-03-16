06199066cd550d4383c5cb195ee2564efc4e194d
Zijian Zhou <yanjingzhaisun@gmail.com>
2019-03-15 23:24:41 -0400

    changed speed

--- string-to-midi.py
+++ string-to-midi.py
@@ -6,7 +6,7 @@ track    = 0
 string   = "Sur le fond de mes nuits Dieu de son doigt savant Dessine un cauchemar multiforme et sans trêve."
 channel  = 0
 time     = 0
-duration = 1 
+duration = 0.5
 tempo    = 166
 volume   = 100 
 string = string.replace(" ", "")
@@ -15,7 +15,7 @@ array = list(string)
 MyMIDI = MIDIFile(1)
 for x in range(0, len(array)):
     print(ord(array[x]))
-    MyMIDI.addNote(track, channel, ord(array[x]), time + x, duration, volume)
+    MyMIDI.addNote(track, channel, ord(array[x]), time + x * duration, duration, volume)
 
 with open("Charles Baudelaire.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
